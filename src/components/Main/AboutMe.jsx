053bd945809dd7f10ba68782dc922cd7f364345f
ashiful-islam <ashifulislam2002@gmail.com>
2024-05-26 13:34:30 +0600

    logo added

--- src/components/Main/AboutMe.jsx
+++ src/components/Main/AboutMe.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import logo from '../../assets/logo.png'
 const AboutMe = () => {
     return (
         <>
             <div>
                 <h1 className='heading1'>Hello there i'm <span>amit hasan</span></h1>
                 <p>Photography is my passion. i love to do this. (add more 400 word...)</p>
+            </div>
+
+            <div>
+                <img src={logo} alt="logo" />
             </div>
         </>
     )
